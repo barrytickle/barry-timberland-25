1f5767d460846b2cf6f29d1169e26a582de0e2f5
Barry Tickle <barry.tickle12@gmail.com>
2025-05-10 01:50:10 +0100

    Fixed bug with assets

--- theme/functions.php
+++ theme/functions.php
@@ -86,11 +86,16 @@ class Timberland extends Timber\Site {
 	}
 
 	public function enqueue_assets() {
-		wp_dequeue_style( 'wp-block-library' );
-		wp_dequeue_style( 'wp-block-library-theme' );
-		wp_dequeue_style( 'wc-block-style' );
-		wp_dequeue_script( 'jquery' );
-		wp_dequeue_style( 'global-styles' );
+		// Prevent dequeueing of critical scripts in admin
+		if (is_admin()) {
+			return;
+		}
+	
+		wp_dequeue_style('wp-block-library');
+		wp_dequeue_style('wp-block-library-theme');
+		wp_dequeue_style('wc-block-style');
+		wp_dequeue_script('jquery');
+		wp_dequeue_style('global-styles');
 
 		
 
